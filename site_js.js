0d25e23a134918c5f862784a913c09c3b8ce1d92
Dielle Oliveira Ribeiro <70522897+Diellita@users.noreply.github.com>
2020-12-14 16:06:50 -0300

    Add files via upload

---
+++ site_js.js
@@ -0,0 +1,13 @@
+
+
+
+//window.addEventListener("load", function(event) {
+    // login ();
+    // document.querySelector('#acessar').addEventListener('click', function() {cadastrar()},false);
+//});
+
+
+
+
+
+
